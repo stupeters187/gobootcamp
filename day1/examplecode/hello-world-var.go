8c625205c862802abf2913b93670281917a8c723
Stu Peters <stu_peters187@hotmail.com>
2018-10-23 16:43:26 -0400

    added scanf exercise

---
+++ day1/examplecode/hello-world-var.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+var s = "stu"
+
+func main()  {
+	fmt.Println(s)
+}
---
+++ day1/examplecode/scanf.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var s string
+
+	fmt.Scanf("%s", &s)
+	fmt.Println("Hello", s)
+}
+
