12bacbb4f01da98da3e931db08ebb1a3ea231777
Stu Peters <stu_peters187@hotmail.com>
2018-10-23 16:09:33 -0400

    day1 ,hello world

---
+++ day1/examplecode/os-exit.go
@@ -0,0 +1,28 @@
+package hello
+
+import (
+	"fmt"
+	"os"
+)
+
+-world
+
+import (
+	"fmt"
+	"os"
+)
+
+
+
+import (
+"fmt"
+"os"
+)
+
+func main()  {
+
+	defer fmt.Println("!")
+
+	os.Exit(187)
+}
+
