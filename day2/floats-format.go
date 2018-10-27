5880f5de57157f004be612e8ce13cd5b0d24825f
Stu Peters <stu_peters187@hotmail.com>
2018-10-27 21:29:55 +0200

    added formatting to floats

---
+++ day2/floats-format.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main()  {
+
+	str := fmt.Sprintf("this is my number: %9.6f", 1.23)
+	fmt.Println(str)
+}
---
+++ day2/test.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+var x = []int{1,2,3,4}
+
+func main()  {
+	fmt.Println(len(x))
+}
